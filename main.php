cef1252e353589d971b70e8a8123400ec022e171
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-06 23:30:53 +0700

    Update main.php

--- main.php
+++ main.php
@@ -2,32 +2,46 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
-$app = new IceTea\HeadlineNewsScraper(
-    new IceTea\SitesHandler\Kompas,
-    new IceTea\SitesHandler\Detik,
-	new IceTea\SitesHandler\Liputan6,
-	new IceTea\SitesHandler\Tribunnews,
-	new IceTea\SitesHandler\CNNIndonesia
-);
+while (true) {
+    $pdo = new PDO("mysql:host=localhost;dbname=test_aa", "root", "858869123");
+    $st = $pdo->prepare("INSERT INTO `news` (`hash`,`site`,`title`,`created_at`) VALUES (:hash, :site, :title, :created_at);");
 
-$app->setInsertAction(function ($instanceName, $results) use ($pdo) {
-    /**
-     * Example :
-     *
-     * $instanceName    "IceTea\SitesHandler\Liputan6"
-     * $results         "Anies Akan Cabut Larangan Motor Lewat Jalan MH Thamrin"
-     */
-    if (is_array($results)) {
+
+    $app = new IceTea\HeadlineNewsScraper(
+        new IceTea\SitesHandler\Kompas,
+        new IceTea\SitesHandler\Detik,
+    	new IceTea\SitesHandler\Liputan6,
+    	new IceTea\SitesHandler\Tribunnews,
+    	new IceTea\SitesHandler\CNNIndonesia
+    );
+    $app->setInsertAction(function ($instanceName, $results) use ($st) {
+        /**
+         * Example :
+         *
+         * $instanceName    "IceTea\SitesHandler\Liputan6"
+         * $results         "Anies Akan Cabut Larangan Motor Lewat Jalan MH Thamrin"
+         */
+        
+        $st->execute(
+            [
+                ":hash" => sha1($result),
+                ":site" => $instanceName,
+                ":title" => $result,
+                ":created_at" => time()
+            ]
+        );
+
         echo $instanceName . " : " . PHP_EOL;
         $i = 1;
         array_walk($results, function ($result) use (&$i) {
             echo ($i++).". ".$result . PHP_EOL;
         });
-    } else {
-        echo $instanceName . " : " . PHP_EOL;
-        echo "1. ".$results . PHP_EOL;
-    }
-    echo PHP_EOL;
-});
+        
+        echo PHP_EOL;
+    });
 
-$app->run();
+    $app->run();
+
+    // delay
+    sleep(60);
+}
