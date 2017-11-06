35e7875f5a85b4273df14564eae38eaab624594c
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-07 00:17:56 +0700

    Fix syntax error

--- wordcloud.php
+++ wordcloud.php
@@ -4,7 +4,7 @@ $st = $pdo->prepare("SELECT `title` FROM `news`;");
 
 $container = [];
 
-while ($st = $st->fetch(PDO::FETCH_NUM) {
+while ($st = $st->fetch(PDO::FETCH_NUM)) {
 	foreach (explode(" ", $st[0]) as $val) {
 		$val = strtolower($val);
 		isset($container[trim($val)]) and $container[$val]++ or $container[$val] = 1;
