2a521e80d749de8ed676322b7c004d446a873e6a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-08 12:37:46 +0700

    Add exclude words

--- wordcloud.php
+++ wordcloud.php
@@ -4,11 +4,14 @@ $st = $pdo->prepare("SELECT `title` FROM `news`;");
 $st->execute();
 
 $container = [];
-
+$exclude = file("stopwordbahasa.csv");
 while ($_st = $st->fetch(PDO::FETCH_NUM)) {
 	foreach (explode(" ", $_st[0]) as $val) {
 		$val = strtolower($val);
-		isset($container[trim($val)]) and $container[$val]++ or $container[$val] = 1;
+		$val = trim(preg_replace("#[^a-z0-9]#", "", $val));
+		if(! in_array($val, $exclude)){
+		isset($container[$val]) and $container[$val]++ or $container[$val] = 1;
+		}
 	}
 }
 $cont = $container;
@@ -16,7 +19,7 @@ rsort($container);
 $i = 1;
 foreach ($container as $key => $value) {
 	echo ($i++).". ".array_search($value, $cont)." -> ".$value." kali" . PHP_EOL;
-	if ($i === 10) {
+	if ($i === 11) {
 		break;
 	}
 }
